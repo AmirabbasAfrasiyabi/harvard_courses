6a3c4c6b6d647b9e3a21e59935ef1408d9541951
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-08-02 10:25:12 +0330

    condition created

---
+++ python/condition/compare.py
@@ -0,0 +1,48 @@
+"""basic example"""
+# x = int(input())
+# y = int(input())
+
+"""solution 1"""
+# if x < y :
+#     print('x is less then y')
+
+# elif x > y :
+#     print('x is greater then y')
+
+# else:
+#     print("x is equal y")
+
+
+"""solution2"""
+
+# if x != y:
+#     print('x is not equal to y')
+# else:
+#     print("x is equal to y")
+
+
+
+"""
+odd and even
+"""
+# def main():
+#     z = int(input())
+    
+#     if is_even(z):
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# def is_even(n):
+#     return True if n % 2 ==0 else False
+# main()
+
+
+"""match-case"""
+
+# name = input()
+# match name :
+#     case "Amir"| "Atiye" :
+#         print("Graffindor")
+#     case _ :
+#         print ("Who?")
