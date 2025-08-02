92ce99ffcf347df49fd456dc26319eb29939505e
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-08-02 11:52:39 +0330

    loop created

---
+++ python/loop/Hagward.py
@@ -0,0 +1,33 @@
+"""list"""
+# student = ["Amir" , "Ati" , "Reyhan"]
+
+# """for loop"""
+# for Students in student:
+#     print(Students)
+
+# """or"""
+# for i in range(len(student)):
+#     print(i , student[i])
+
+
+"""dict"""
+
+# student = {"Amir" : "09120910309" ,
+#            "Ati" : "09120034660" , 
+#            "Reyhan" : "09126993119"
+#            }
+
+# for students in student:
+#     print(students ,student[students] )
+ 
+"""EXAMPLE"""
+
+# students = [
+#     {"Amir" : "09120910309" ,"Ati" : "09120034660" , "Reyhan" : "09126993119"} , 
+#     {"Mather" : "09120781057" ,"Father" : "09128433591"}
+# ]
+
+# for i in students:
+#     print(i)
+
+ 
---
+++ python/loop/cat.py
@@ -0,0 +1,28 @@
+
+"""while input"""
+# while True :
+#     n = int(input())
+#     if n <0:
+#         continue
+#     else:
+#         break
+# for i in range(n):
+#     print("meow")
+
+
+"""function"""
+
+# def main():
+#     number = get_number()
+#     meow(number)
+
+# def get_number():
+#     while True:
+#         n = int(input())
+#         if n > 0:
+#             break
+#     return n
+
+# def meow(n):
+#     for i in range(n):
+#         print("meow")
