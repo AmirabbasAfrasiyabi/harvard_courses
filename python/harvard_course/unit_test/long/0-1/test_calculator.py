ce8547f47b31b59edfdf3e60f759e34d0ee29ea1
Amirabbas Afrasiyabi <abb.afrasiyabi@gmail.com>
2025-11-07 16:48:17 +0330

    unit test created

---
+++ python/harvard_course/unit_test/long/0-1/test_calculator.py
@@ -0,0 +1,18 @@
+import  pytest
+from calculator import *
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+def test_negative():
+    assert square(-3) == 9
+    assert square(-2) == 4
+
+
+def test_zero():
+    assert square(0) == 0
+
+def test_string():
+    with pytest.raises(TypeError):
+        square("string")
