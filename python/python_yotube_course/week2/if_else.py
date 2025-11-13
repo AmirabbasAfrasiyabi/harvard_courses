40c30fbece05efcb807a95442a87210d67da9f05
Amirabbas Afrasiyabi <abb.afrasiyabi@gmail.com>
2025-11-13 11:07:34 +0330

    third course created

---
+++ python/python_yotube_course/week2/if_else.py
@@ -0,0 +1,67 @@
+"""if_else"""
+from tkinter.font import names
+
+# x = int(input("x :"))
+# y = int(input("y : "))
+
+
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("x is less than y")
+# else:
+#     print("x is equal to y")
+
+
+
+# if x!=y:
+#     print("x is not equal  y")
+# else:
+#     print("x is equal  y")
+
+
+
+"""grade"""
+# if 100 >= x >= 90:
+#     print("grade A")
+# elif 90 >= x >= 80:
+#     print("grade B")
+# else:
+#     print("grade C")
+
+
+
+"""even_odd"""
+# if x % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+"""evan_odd with function"""
+# def main():
+#     x = int(input('x:'))
+#     if evan_odd_digit(x) :
+#         print('even')
+#     else:
+#         print('odd')
+#
+# def evan_odd_digit(x):
+#     return True if x % 2 == 0 else False
+#
+# main()
+
+
+
+"""match_case"""
+# name = input("Enter your name: ").strip().title()
+# match name:
+#     case "Harry" |"Ron" | "Hermine":
+#         print("G")
+#     case "abbas":
+#         print("B")
+#     case _:
+#         print("whi ??")
+
+
---
+++ python/python_yotube_course/week3/loop.py
@@ -0,0 +1,69 @@
+import time
+"""while loop"""
+# num= 3
+# while num !=0 :
+#     num -=1
+#     print("hi")
+# print("finished")
+
+
+# i = 0
+# while True:
+#     print(i)
+#     i += 1
+#     if i == 110:
+#         break
+# print("finished")
+
+
+"""for loop"""
+# for i in range(1,100,1):
+#     if i%2==0:
+#         print(f"{i} is even")
+#     else:
+#         print(f"{i} is odd")
+
+
+# for i in range(5):
+#     print(i)
+#     time.sleep(3)
+
+
+# while True:
+#     a = int(input())
+#     if a>0 :
+#         break
+#     else:
+#         continue
+# print(a)
+
+
+
+
+"""list"""
+# student = ["amir" , "ati" , "rey" , "Hamide" , "mansor"]
+
+# for i in range (len(student)):
+#     print(i+1 , student[i])
+
+# for i in student:
+#     print(i)
+
+# student.append("Abbas")
+# print(student)
+
+
+"""dictionary"""
+
+# d = {"Amir" : "09120910309" ,
+#      "Ati" : "09120034660" ,
+#      "Reyhan" : "091269931119" ,
+#      "Mansor" : "09128433591" ,
+#      "Hamide" : "09120781057"}
+#
+# for i in d :
+#     print(i , d[i] , sep = " : ")
+
+
+# for i in d.items():
+#     print(i)
