81531197b611daeb8e87c1cdfd2462b3b632162a
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-11-30 21:50:03 +0330

    updated week 6

--- python/python_yotube_course/week6/file.py
+++ python/python_yotube_course/week6/file.py
@@ -5,27 +5,35 @@
 # for name in sorted(nums):
 #     print(f'hello {name}')
 
-"""example 2"""
+
+"""example 2 -> write a file with w method """
 # name = input("Enter your name: ")
 # file = open("file.txt", "w")
 # file.write(name)
 # file.close()
 
-"""example 3"""
+"""example 3 -> write any example  -> not good"""
+# name = input("Enter your name: ")
+# file = open("file.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+
+"""example 4 -> solve problem example3 -> 1- write with and delete line 19"""
 # name = input("Enter your name: ")
 # with open("file.txt", "a") as file:
 #     file.write(f"{name}\n")
 
 
-"""example 4"""
-
+"""example 4 -> print hello to all file.txt user and line"""
 # with open("file.txt") as file:
 #     lines = file.readlines()
+#     print(lines)
 # for line in sorted(lines):
 #     print("hello" , line.rstrip())
 
 
-"""example 5"""
+"""example 5 -> Append name to a list for sorting"""
 
 # names= []
 # with open("file.txt" ) as file:
@@ -35,28 +43,33 @@
 #     print(f"hello {name}")
 
 
-"""example 6 csv files"""
-# student = []
+"""csvfile"""
+# students = []
 # with open("file.csv") as file:
 #     for line in file:
-#         name , house , years = line.strip().split(',')
-#         student.append(f"{name} is in {house} , and {years}")
+#         name,house = line.strip().split(',')
+#         student= {'name': name, 'house': house}
+#         students.append(student)
 #
-# for _ in sorted(student):
-#     print(_)
+# def get_name(student):
+#     return student['name']
+#
+# for student in sorted(students , key = lambda student: student['house']):
+#     print(f'name: {student["name"]}, is in  {student["house"]}')
 
 
-"""example 7"""
-# import csv
-# student = []
-# with open('file.csv', newline='') as file:
-#     reader = csv.reader(file, delimiter='\t')
-#     for row in reader:
-#         if len(row) != 3:
-#             continue
-#         student.append({'name': name, 'house': home , 'years': age})
-# for student in sorted(student , key=lambda student: student['name']):
-#     print(f"{student['name']} is {student['years']} years old.")
+
+"""example2"""
+import csv
+students = []
+with open("file.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        student= {'name': row[0], 'house': row[1]}
+        students.append(student)
+
+for student in sorted(students , key = lambda student: student['house']):
+    print(f'name: {student["name"]}, is in  {student["house"]}')
 
 
 
